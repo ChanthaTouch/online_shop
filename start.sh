f910ccb544a0005cffea3f8fbf0b5e3bfda3c600
chantha <chantha.touch@futura-dnc.com>
2026-02-13 16:38:00 +0700

    update

--- start.sh
+++ start.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# Exit immediately if a command fails
 set -e
 
-# Step 1: Start Laravel backend only
 cd store-backend
-composer install --no-dev --optimize-autoloader
 php artisan migrate --force
 php artisan serve --host=0.0.0.0 --port=8000
