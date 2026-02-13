d0f210bfba1d70a207860807c5a4925a8e273225
chantha <chantha.touch@futura-dnc.com>
2026-02-13 22:44:00 +0700

    add start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,4 @@
-#!/bin/bash
-# Exit immediately if a command fails
-set -e
+composer install --optimize-autoloader --no-dev
 
-# Step 1: Build Vue frontend
-cd frontend
-npm install
-npm run build
-
-# Step 2: Copy built Vue assets into Laravel public folder
-cp -r dist/* ../store-backend/public/
-
-# Step 3: Start Laravel backend
-cd store-backend
- composer install --no-dev --optimize-autoloader 
- php artisan migrate --force 
- php artisan serve --host=0.0.0.0 --port=8000
+php artisan migrate --force
+php artisan optimize
