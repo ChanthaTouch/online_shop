74c2aae4dd2fca68ca54b2f96dafe1e5dee360f5
chantha <chantha.touch@futura-dnc.com>
2026-02-20 15:02:00 +0700

    update

--- store-backend/config/cors.php
+++ store-backend/config/cors.php
@@ -10,7 +10,7 @@ return [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'http://localhost:8000',
-        'https://awake-comfort-production.up.railway.app',
+        'https://onlineshop-production-f1eb.up.railway.app',
     ],
 
     'allowed_origins_patterns' => [
